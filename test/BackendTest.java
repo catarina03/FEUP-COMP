cc41f216054c6b094eae7d0fa6bae974b945b48e
Teresa <tiscorado@gmail.com>
2021-04-30 04:09:54 +0100

    Started jasmin

--- test/BackendTest.java
+++ test/BackendTest.java
@@ -17,7 +17,11 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 import pt.up.fe.comp.TestUtils;
+import pt.up.fe.comp.jmm.ollir.OllirResult;
+import pt.up.fe.comp.jmm.ollir.OllirUtils;
 import pt.up.fe.specs.util.SpecsIo;
+
+import java.util.ArrayList;
 
 public class BackendTest {
 
@@ -29,4 +33,12 @@ public class BackendTest {
         var output = result.run();
         assertEquals("Hello, World!", output.trim());
     }
+
+    @Test
+    public void testOllir() {
+        var result = TestUtils.backend(new OllirResult(OllirUtils.parse(SpecsIo.getResource("fixtures/public/HelloWorld.jmm")),
+        null, new ArrayList<>()));
+
+    }
+
 }
